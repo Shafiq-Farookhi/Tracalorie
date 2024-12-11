384e067b72317455ea6838c2d90aa127f19859fd
Shafiq <Shahramfarokhi421@gmail.com>
2024-12-11 14:54:46 +0430

    Daily limit added

--- js/app.js
+++ js/app.js
@@ -59,6 +59,13 @@ class CalorieTracker {
         this._render();
     }
 
+    setLimit(calorieLimit) {
+        this._calorieLimit = calorieLimit;
+        this._displayCaloriesLimit();
+        this._render();
+
+    }
+
     // private Methods/API
 
     _displayCaloriesTotal() {
@@ -202,6 +209,7 @@ class App {
         document.getElementById('filter-workouts').addEventListener('keyup', this._filterItems.bind(this, 'workout'));
 
         document.getElementById('reset').addEventListener('click', this._reset.bind(this));
+        document.getElementById('limit-form').addEventListener('submit', this._setLimit.bind(this));
     }
 
     _newMeal(e) {
@@ -284,7 +292,24 @@ class App {
         document.getElementById('workout-items').innerHTML = '';
         document.getElementById('filter-meals').value = '';
         document.getElementById('filter-workouts').value = '';
+    }
 
+    _setLimit(e){
+        e.preventDefault();
+
+        const limit = document.getElementById('limit');
+
+        if (limit.value === '') {
+            alert('Please add a limit!')
+            return;
+        }
+
+        this._tracker.setLimit(+limit.value);
+        limit.value = '';
+
+        const modalEl = document.getElementById('limit-modal');
+        const modal = bootstrap.Modal.getInstance(modalEl);
+        modal.hide();
     }
 }
 
