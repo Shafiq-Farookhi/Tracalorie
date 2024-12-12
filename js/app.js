736646d8245cff293315e037a0c4e79139125f92
Shafiq <Shahramfarokhi421@gmail.com>
2024-12-12 14:36:19 +0430

    Save workouts to Local Storage

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ class CalorieTracker {
         this._calorieLimit = Storage.getCalorieLimit();
         this._totalCalories = Storage.getTotalCalories();
         this._meals = Storage.getMeals();
-        this._workouts = [];
+        this._workouts = Storage.getWorkouts();
 
         this._displayCaloriesTotal();
         this._displayCaloriesLimit();
@@ -29,6 +29,7 @@ class CalorieTracker {
         this._workouts.push(workout);
         this._totalCalories -= workout.calories;
         Storage.updateTotalCalories(this._totalCalories);
+        Storage.saveworkout(workout);
         this._displayNewWorkout(workout);
         this._render();
     }
@@ -74,7 +75,8 @@ class CalorieTracker {
     }
 
     loadItems() {
-        this._meals.forEach(meal => this._displayNewMeal(meal))
+        this._meals.forEach(meal => this._displayNewMeal(meal));
+        this._workouts.forEach(workout => this._displayNewWorkout(workout));
     }
 
     // private Methods/API
@@ -249,6 +251,22 @@ class Storage{
         localStorage.setItem('meals', JSON.stringify(meals));
     }
 
+    static getWorkouts(){
+        let workouts;
+        if (localStorage.getItem('workouts') === null) {
+            workouts = [];
+        } else {
+            workouts = JSON.parse(localStorage.getItem('workouts'));
+        }
+        return workouts;
+    }
+
+    static saveworkout(workout){
+        const workouts = Storage.getWorkouts();
+        workouts.push(workout);
+        localStorage.setItem('workouts', JSON.stringify(workouts));
+    }
+
 }
 
 
@@ -256,7 +274,7 @@ class App {
     constructor() {
         this._tracker = new CalorieTracker();
         this._loadEventListeners();
-        this._tracker.loadItems();
+        this._tracker.loadItems(); 
     }
 
     _loadEventListeners(){
